a4c293b4f46c0ef79fa3598fc8fd8f91739dfefa
Bo-Yi.Wu <appleboy.tw@gmail.com>
2023-03-12 10:04:02 +0800

    refactor: add `git` dependency and optional `zsh` dependency to formula
    - Remove bash and zsh completion installation in the formula
    - Add dependency on `git`
    - Add optional dependency on `zsh`

--- codegpt.rb
+++ codegpt.rb
@@ -7,34 +7,10 @@ class Codegpt < Formula
     if Hardware::CPU.intel?
       url "https://github.com/appleboy/CodeGPT/releases/download/v0.0.7/CodeGPT-0.0.7-darwin-amd64"
       sha256 "a415dea29c21dbc50cb6f77319a07ad76f9d722119b85277bc13aced06581a65"
-
-      def install
-        bin.install "codegpt"
-
-        # Install bash completion
-        output = Utils.safe_popen_read("#{bin}/codegpt", "completion", "bash")
-        (bash_completion/"codegpt").write output
-
-        # Install zsh completion
-        output = Utils.safe_popen_read("#{bin}/codegpt", "completion", "zsh")
-        (zsh_completion/"_codegpt").write output
-      end
     end
     if Hardware::CPU.arm?
       url "https://github.com/appleboy/CodeGPT/releases/download/v0.0.7/CodeGPT-0.0.7-darwin-arm64"
       sha256 "c5f3e7d3013e7857233afe3dc6a2b914513d22dfce9d7a4a07bb81965d986d4d"
-
-      def install
-        bin.install "codegpt"
-
-        # Install bash completion
-        output = Utils.safe_popen_read("#{bin}/codegpt", "completion", "bash")
-        (bash_completion/"codegpt").write output
-
-        # Install zsh completion
-        output = Utils.safe_popen_read("#{bin}/codegpt", "completion", "zsh")
-        (zsh_completion/"_codegpt").write output
-      end
     end
   end
 
@@ -42,38 +18,27 @@ class Codegpt < Formula
     if Hardware::CPU.intel?
       url "https://github.com/appleboy/CodeGPT/releases/download/v0.0.7/CodeGPT-0.0.7-linux-amd64"
       sha256 "99d183d8bc36a0c9bed9ef5efc1f640e1e553367752e505621570ece614a9136"
-
-      def install
-        bin.install "codegpt"
-
-        # Install bash completion
-        output = Utils.safe_popen_read("#{bin}/codegpt", "completion", "bash")
-        (bash_completion/"codegpt").write output
-
-        # Install zsh completion
-        output = Utils.safe_popen_read("#{bin}/codegpt", "completion", "zsh")
-        (zsh_completion/"_codegpt").write output
-      end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/appleboy/CodeGPT/releases/download/v0.0.7/CodeGPT-0.0.7-linux-arm64"
       sha256 "6def3f20cc60d898a1a949516bac2caccbf180421d0f58fc3132c0b3bba769a4"
-
-      def install
-        bin.install "codegpt"
-
-        # Install bash completion
-        output = Utils.safe_popen_read("#{bin}/codegpt", "completion", "bash")
-        (bash_completion/"codegpt").write output
-
-        # Install zsh completion
-        output = Utils.safe_popen_read("#{bin}/codegpt", "completion", "zsh")
-        (zsh_completion/"_codegpt").write output
-      end
     end
   end
 
   depends_on "git"
+  depends_on "zsh" => :optional
+
+  def install
+    bin.install "codegpt"
+
+    # Install bash completion
+    output = Utils.safe_popen_read("#{bin}/codegpt", "completion", "bash")
+    (bash_completion/"codegpt").write output
+
+    # Install zsh completion
+    output = Utils.safe_popen_read("#{bin}/codegpt", "completion", "zsh")
+    (zsh_completion/"_codegpt").write output
+  end
 
   test do
     system "#{bin}/codegpt version"
