3e5fb7c3774f483608028a72a988f2b5b4441b6c
Bo-Yi.Wu <appleboy.tw@gmail.com>
2023-04-02 18:06:12 +0800

    feat: add formula for drone-lambda binary to Homebrew
    - Add a new formula for `drone-lambda.rb`
    - Set the `desc`, `homepage`, and `version` values
    - Set the `os`, `arch`, and `filename` values
    - Set the `url`, `using`, and `sha256` values
    - Add `zsh` as an optional dependency
    - Install `drone-lambda` as a binary
    - Add a test for `drone-lambda` version command
    
    Signed-off-by: Bo-Yi.Wu <appleboy.tw@gmail.com>

---
+++ drone-lambda.rb
@@ -0,0 +1,41 @@
+class DroneLambda < Formula
+  desc "Execute commands on a remote host through SSH"
+  homepage "https://github.com/appleboy/drone-lambda"
+  version "1.3.45"
+
+  os = OS.mac? ? "darwin" : "linux"
+  arch = case Hardware::CPU.arch
+         when :x86_64 then "amd64"
+         when :arm64 then "arm64"
+         else
+           raise "drone-lambda: Unsupported system architecture #{Hardware::CPU.arch}"
+         end
+
+  @@filename = "drone-lambda-#{version}-#{os}-#{arch}"
+  @@url = "https://github.com/appleboy/drone-lambda/releases/download/v#{version}/#{@@filename}"
+  @@using = :nounzip
+
+  @@sha256 = case "#{os}-#{arch}"
+    when "linux-amd64" then "3a1fdc584b380e97f13b8a15d23448f011085e936886b3dc1ff607ca705f4715"
+    when "linux-arm64" then "49ff95f2a26232e22c757024191c495903f0efaa987ee22f9e652d35d5abfdc2"
+    when "darwin-amd64" then "f3c11a9525ede49839da4079444587dead76be79daad1b9c94e970bd97c377ce"
+    when "darwin-arm64" then "efb7f79123fd41f792f28d39383aec0699389fa20952b50524496d84f8eb8521"
+    else
+      raise "drone-lambda: Unsupported system #{os}-#{arch}"
+    end
+
+  sha256 @@sha256
+  url @@url,
+    using: @@using
+
+  depends_on "zsh" => :optional
+
+  def install
+    filename = DroneLambda.class_variable_get("@@filename")
+    bin.install filename => "drone-lambda"
+  end
+
+  test do
+    system "#{bin}/drone-lambda", "version"
+  end
+end
