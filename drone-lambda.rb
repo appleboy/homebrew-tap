b1390e06c70c2255aef29829156b1b6cbc01aaea
Bo-Yi.Wu <appleboy.tw@gmail.com>
2023-04-04 06:48:58 +0800

    chore: update sha256 hash for all supported OSs
    - Update the sha256 hash for linux-amd64
    - Update the sha256 hash for linux-arm64
    - Update the sha256 hash for darwin-amd64
    - Update the sha256 hash for darwin-arm64
    
    Signed-off-by: Bo-Yi.Wu <appleboy.tw@gmail.com>

--- drone-lambda.rb
+++ drone-lambda.rb
@@ -16,10 +16,10 @@ class DroneLambda < Formula
   @@using = :nounzip
 
   @@sha256 = case "#{os}-#{arch}"
-    when "linux-amd64" then "3a1fdc584b380e97f13b8a15d23448f011085e936886b3dc1ff607ca705f4715"
-    when "linux-arm64" then "49ff95f2a26232e22c757024191c495903f0efaa987ee22f9e652d35d5abfdc2"
-    when "darwin-amd64" then "f3c11a9525ede49839da4079444587dead76be79daad1b9c94e970bd97c377ce"
-    when "darwin-arm64" then "efb7f79123fd41f792f28d39383aec0699389fa20952b50524496d84f8eb8521"
+    when "linux-amd64" then "a24b82c7cfe0a3c59f95ef2f178be6fa3c0e23d5065071962cbc25dcc08f0087"
+    when "linux-arm64" then "1102555a54f70475ee73e1681df5d1fe9f1cbd31e7ec86032ba122b8f46cd71b"
+    when "darwin-amd64" then "19e8a367530f06ee0c988ef3c82ceae98d07ec3f78374296b22254e845f2be89"
+    when "darwin-arm64" then "b1a0a5c6bbbd921d289170738cc0324886d24d330ce60e102d8deff91ab1add8"
     else
       raise "drone-lambda: Unsupported system #{os}-#{arch}"
     end
