8f7fc55916d7f3f6de420c84a1196e887f4813e7
Bo-Yi Wu <appleboy.tw@gmail.com>
2023-04-08 10:41:23 +0800

    chore: update version and sha256 hashes for different architectures
    - Update the version from `0.1.5` to `0.1.7`
    - Update the sha256 hashes for the different architectures
    
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- codegpt.rb
+++ codegpt.rb
@@ -1,7 +1,7 @@
 class Codegpt < Formula
   desc "A CLI written in Go language that writes git commit messages for you using ChatGPT AI (gpt-3.5-turbo model) and automatically installs a git prepare-commit-msg hook."
   homepage "https://github.com/appleboy/CodeGPT"
-  version "0.1.5"
+  version "0.1.7"
 
   os = OS.mac? ? "darwin" : "linux"
   arch = case Hardware::CPU.arch
@@ -16,10 +16,10 @@ class Codegpt < Formula
   @@using = :nounzip
 
   @@sha256 = case "#{os}-#{arch}"
-    when "linux-amd64" then "e4bc41117e8bca879ba67027d8e51a627ac043dc59aac5564de72f47599c683f"
-    when "linux-arm64" then "d2a8d357d43e4fcd0b43fe7cfdefb831e289c126c5671ab677a9eb131a001acd"
-    when "darwin-amd64" then "28bd32894a199605c091b697f30f2874aa8135beedff12bcfe8c2f290864c6d0"
-    when "darwin-arm64" then "fb031a59494f68ca35d2070697288d60d64fb9a3f75fdd1122fb21cd5440aceb"
+    when "linux-amd64" then "2232fd87b39424b685da5a1e33f200d8d0d6827494fa612cf47646321351057c"
+    when "linux-arm64" then "884f14b77c64d6fd0b58ca04e3f3c847a026dc44e347774f26dda463192d5863"
+    when "darwin-amd64" then "b8162f8ddc80f1774f5f39093688fb3f1b0903361b22cadd3668736af077c355"
+    when "darwin-arm64" then "c75d38319cfbc6dbd83f33c50b17cb09cb26880c6f4f89e73c0cf4f4b4e19eb2"
     else
       raise "CodeGPT: Unsupported system #{os}-#{arch}"
     end
