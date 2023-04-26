29186f6e3a81a0c7994c571b95c866c6a4d16827
appleboy <appleboy.tw@gmail.com>
2023-04-26 17:23:30 +0800

    chore: improve API usage and test compatibility across OSs
    - Update version from `0.1.9` to `0.2.0`
    - Update checksums for linux-amd64, linux-arm64, darwin-amd64, and darwin-arm64
    
    Signed-off-by: appleboy <appleboy.tw@gmail.com>

--- codegpt.rb
+++ codegpt.rb
@@ -1,7 +1,7 @@
 class Codegpt < Formula
   desc "A CLI written in Go language that writes git commit messages for you using ChatGPT AI (gpt-3.5-turbo model) and automatically installs a git prepare-commit-msg hook."
   homepage "https://github.com/appleboy/CodeGPT"
-  version "0.1.9"
+  version "0.2.0"
 
   os = OS.mac? ? "darwin" : "linux"
   arch = case Hardware::CPU.arch
@@ -16,10 +16,10 @@ class Codegpt < Formula
   @@using = :nounzip
 
   @@sha256 = case "#{os}-#{arch}"
-    when "linux-amd64" then "c28c7568765b6782afe096f7cad2e3496e511179a20d06063263114e0c7b3ca2"
-    when "linux-arm64" then "5bedd8b7ae25316c8299994091f030dc37fffd7fb5f43de561e3e55ff2968073"
-    when "darwin-amd64" then "e3f44a3282075d7a0062119b6a38f4b790f3c01716d50eb5dd34bb49ce580e2a"
-    when "darwin-arm64" then "f0ab8e9954ef3c08843979c68f487ca49711c7d1aa794122c1663fda6af6f3d1"
+    when "linux-amd64" then "6258af5e986b81665a53001717a58e8505131d08668f151740a3e4a26c6376a1"
+    when "linux-arm64" then "6025bc2eacc5707192f2aa0df0b010458c69a87c1cf3968df6367cc05b2f09b4"
+    when "darwin-amd64" then "51aac41f627c433a354a71df9b0817d7e872a81b0377524b14e29a7959bcc4f9"
+    when "darwin-arm64" then "7d65f2d11b88cd6ae46dfddee2bb261faf3b596a1cf12f3d311f5315c14ea0ed"
     else
       raise "CodeGPT: Unsupported system #{os}-#{arch}"
     end
