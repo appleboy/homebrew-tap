9ae52ef679e99186b622d140ab742f7c6f667a2f
Bo-Yi Wu <appleboy.tw@gmail.com>
2023-05-02 11:24:43 +0800

    style: improve output messages and test coverage
    - Modify echo statements to include binary name and OS in version and sha256 update messages
    
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -13,13 +13,13 @@ for bin in ${binaries};do
 	esac
 	latest=$(curl -skL -o /dev/null -w %{url_effective} "${git_url}/releases/latest")
 	version="${latest##*/v}"
-  echo "version: ${version}"
+  echo "update ${bin} version: ${version}"
 
 	file_url="https://github.com/appleboy/${bin}/releases/download/v${version}"
 	for os in ${supported_os}; do
 		sha256_file="checksums.txt"
 		sha256=$(curl -skL "${file_url}/${sha256_file}" | grep ${os}.xz | awk '{print$1}')
-    echo "sha256: ${sha256}"
+    echo "update ${bin}, os: ${os}, sha256: ${sha256}"
 		sed -r "s/^(\s+when \"${os}\" then).*\"(.*)$/\1 \"${sha256}\"\2/" -i "${file}"
 	done
 	sed -r "s/^(\s+version).*/\1 \"${version}\"/" -i drone-lambda.rb
