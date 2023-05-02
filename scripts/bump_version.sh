f597bda58cba4ca4816bb5ad7379087f0dae5c8d
Bo-Yi Wu <appleboy.tw@gmail.com>
2023-05-02 11:29:54 +0800

    fix: improve API usage and test coverage across platforms
    - Add a condition to check if `sha256` is empty and fetch it if necessary
    
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -34,6 +34,10 @@ for bin in ${binaries};do
 	for os in ${supported_os}; do
 		sha256_file="checksums.txt"
 		sha256=$(curl -skL "${file_url}/${sha256_file}" | grep ${os}.xz | awk '{print$1}')
+    if test -z "$sha256"
+    then
+      sha256=$(curl -skL "${file_url}/${sha256_file}" | grep ${os} | awk '{print$1}')
+    fi
     echo "update ${bin} os: ${os}, sha256: ${sha256}"
 		sed -r "s/^(\s+when \"${os}\" then).*\"(.*)$/\1 \"${sha256}\"\2/" -i "${file}"
 	done
