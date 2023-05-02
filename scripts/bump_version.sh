dd1248620f547ed32e2f4fb9129418249177cb93
Bo-Yi Wu <appleboy.tw@gmail.com>
2023-05-02 11:10:28 +0800

    chore: improve compatibility and testing across OSs
    - Use `-skL` flag in curl commands to ignore SSL certificate validation
    - Update sha256 fetching to filter by `${os}.xz` for specific OS
    - Replace usage of `-i.bak` and use `-i` in sed commands for in-place editing
    
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -11,16 +11,16 @@ for bin in ${binaries};do
 			>&2 echo "Error: unrecognized binary ($bin)"
 			exit 1;;
 	esac
-	latest=$(curl -sL -o /dev/null -w %{url_effective} "${git_url}/releases/latest")
+	latest=$(curl -skL -o /dev/null -w %{url_effective} "${git_url}/releases/latest")
 	version="${latest##*/v}"
   echo "version: ${version}"
 
 	file_url="https://github.com/appleboy/${bin}/releases/download/v${version}"
 	for os in ${supported_os}; do
 		sha256_file="checksums.txt"
-		sha256=$(curl -sL "${file_url}/${sha256_file}" | awk '{print$1}')
+		sha256=$(curl -skL "${file_url}/${sha256_file}" | grep ${os}.xz | awk '{print$1}')
     echo "sha256: ${sha256}"
-		sed -i.bak -r "s/(\s*when \"${os}\" then).*\"(.*)$/\1 \"${sha256}\"\2/" "${file}"
+		sed -r "s/^(\s+when \"${os}\" then).*\"(.*)$/\1 \"${sha256}\"\2/" -i "${file}"
 	done
-	sed -i.bak -r "s/^(\s*version).*/\1 \"${version}\"/" "${file}"
+	sed -r "s/^(\s+version).*/\1 \"${version}\"/" -i drone-lambda.rb
 done
