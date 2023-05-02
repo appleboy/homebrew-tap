3d11e22362e02a8f1ce35e635165176a4ac95095
Bo-Yi Wu <appleboy.tw@gmail.com>
2023-05-02 11:27:08 +0800

    chore: improve version bump script and binary support
    - Add multiple binaries to the bump_version script: codegpt, drone-git-push, drone-scp, drone-ssh, and gorush
    - Define their git_url and supported_os for each new binary
    
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
 
-binaries="drone-lambda"
+binaries="drone-lambda codegpt drone-git-push drone-scp drone-ssh gorush"
 for bin in ${binaries};do
 	file="${bin}.rb"
 	case "$bin" in
 		drone-lambda)
 			git_url="https://github.com/appleboy/drone-lambda"
+			supported_os="linux-amd64 linux-arm64 darwin-amd64 darwin-arm64";;
+		codegpt)
+			git_url="https://github.com/appleboy/codegpt"
+			supported_os="linux-amd64 linux-arm64 darwin-amd64 darwin-arm64";;
+		drone-git-push)
+			git_url="https://github.com/appleboy/drone-git-push"
+			supported_os="linux-amd64 linux-arm64 darwin-amd64 darwin-arm64";;
+		drone-scp)
+			git_url="https://github.com/appleboy/drone-scp"
+			supported_os="linux-amd64 linux-arm64 darwin-amd64 darwin-arm64";;
+		drone-ssh)
+			git_url="https://github.com/appleboy/drone-ssh"
+			supported_os="linux-amd64 linux-arm64 darwin-amd64 darwin-arm64";;
+		gorush)
+			git_url="https://github.com/appleboy/gorush"
 			supported_os="linux-amd64 linux-arm64 darwin-amd64 darwin-arm64";;
 		*)
 			>&2 echo "Error: unrecognized binary ($bin)"
@@ -19,7 +34,7 @@ for bin in ${binaries};do
 	for os in ${supported_os}; do
 		sha256_file="checksums.txt"
 		sha256=$(curl -skL "${file_url}/${sha256_file}" | grep ${os}.xz | awk '{print$1}')
-    echo "update ${bin}, os: ${os}, sha256: ${sha256}"
+    echo "update ${bin} os: ${os}, sha256: ${sha256}"
 		sed -r "s/^(\s+when \"${os}\" then).*\"(.*)$/\1 \"${sha256}\"\2/" -i "${file}"
 	done
 	sed -r "s/^(\s+version).*/\1 \"${version}\"/" -i drone-lambda.rb
