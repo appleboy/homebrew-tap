0a4c3a5c006948efc7e0accd29d284274863d452
Bo-Yi Wu <appleboy.tw@gmail.com>
2023-05-29 05:33:58 +0000

    [skip ci] Updated Version via cron

--- deploy-k8s.rb
+++ deploy-k8s.rb
@@ -1,7 +1,7 @@
 class DeployK8s < Formula
   desc "Generate a Kubeconfig or creating & updating K8s Deployments."
   homepage "https://github.com/appleboy/deploy-k8s"
-  version "0.0.1"
+  version "0.0.2"
 
   os = OS.mac? ? "darwin" : "linux"
   arch = case Hardware::CPU.arch
@@ -18,10 +18,10 @@ class DeployK8s < Formula
   depends_on "xz"
 
   @@sha256 = case "#{os}-#{arch}"
-    when "linux-amd64" then "561d39ef7d3b13e486edfbfd4809ba29134ed8980f0bf33c3e64f005f38bb52f"
-    when "linux-arm64" then "3e760b7513fb9c0ca1f2419898b02417b596d0bcd181f9e58f065fe7e4530c47"
-    when "darwin-amd64" then "55a530538f494c6e7a2575809ddda452d3c288fe2f5589ab8f7ed487b37726ed"
-    when "darwin-arm64" then "d0b2124342aca7c9c06fb6af65c66781a885a0426f7f2ffc89785bed714c6808"
+    when "linux-amd64" then "29416caa429d1b9b8327dbebf8dc2dddec0ab8ca7e8530ab534b938544528ada"
+    when "linux-arm64" then "d74bf0c4cc1cf8b7f55863b5b517e7d5131f0d671b7fefa525b7ecdf76ea6190"
+    when "darwin-amd64" then "2e69d2f4ae6d36d780aef01de7a23a0c659e8c39323223cb49b9b7293b6d7544"
+    when "darwin-arm64" then "9329348d03f6f015e5ea39e2338742944a4ea807cde2e70dea59ffa10bb162b6"
     else
       raise "deploy-k8s: Unsupported system #{os}-#{arch}"
     end
