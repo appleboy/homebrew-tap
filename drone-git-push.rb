bd938e62ce78967bfc44020e20f7a5d0dae9322b
Bo-Yi Wu <appleboy.tw@gmail.com>
2023-07-02 05:33:47 +0000

    [skip ci] Updated Version via cron

--- drone-git-push.rb
+++ drone-git-push.rb
@@ -1,7 +1,7 @@
 class DroneGitPush < Formula
   desc "Drone plugin for deploying code using git push"
   homepage "https://github.com/appleboy/drone-git-push"
-  version "1.0.4"
+  version "1.0.5"
 
   os = OS.mac? ? "darwin" : "linux"
   arch = case Hardware::CPU.arch
@@ -18,10 +18,10 @@ class DroneGitPush < Formula
   depends_on "xz"
 
   @@sha256 = case "#{os}-#{arch}"
-    when "linux-amd64" then "1d0274e679184bff298f1528c57e7d4bb58b59321004dd09d1fcc736f5bd237d"
-    when "linux-arm64" then "6d2f5b5114866819169cdeda8245ee44dbe9163626373a1af4471f99f9e09f90"
-    when "darwin-amd64" then "dbb076421c0fcc9283006f31423f4093756cada51d0aa5e0c805b01a35b9cf01"
-    when "darwin-arm64" then "f5568d02ef6cdd5ac29127082ab849f64cb502030c9c84bcceaf8f03af06ae62"
+    when "linux-amd64" then "a4a9a4864afd2fd7bc9d56db675d7164a2a2714b01d866d74f8a5d6c4367f99f"
+    when "linux-arm64" then "3c269955afce253498b8671194b4cfd1e18ba8a3d0034fb5c2bb67ee473787bf"
+    when "darwin-amd64" then "890e9173f2cb29d4b3170af0ff97661e12c86bbff605da746f35498f10933048"
+    when "darwin-arm64" then "871fae48d012723371010435753121dad8c24ece3e5eb4ff2d8a73cb48e18a54"
     else
       raise "drone-git-push: Unsupported system #{os}-#{arch}"
     end
