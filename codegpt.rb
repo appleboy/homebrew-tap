e63871c32eb816bb6fabe29c930bf816ce50f444
Bo-Yi Wu <appleboy.tw@gmail.com>
2023-07-30 23:03:32 +0000

    [skip ci] Updated Version via cron

--- codegpt.rb
+++ codegpt.rb
@@ -1,7 +1,7 @@
 class Codegpt < Formula
   desc "A CLI written in Go language that writes git commit messages for you using ChatGPT AI (gpt-3.5-turbo model) and automatically installs a git prepare-commit-msg hook."
   homepage "https://github.com/appleboy/CodeGPT"
-  version "0.2.2"
+  version "0.3.0"
 
   os = OS.mac? ? "darwin" : "linux"
   arch = case Hardware::CPU.arch
@@ -18,10 +18,10 @@ class Codegpt < Formula
   depends_on "xz"
 
   @@sha256 = case "#{os}-#{arch}"
-    when "linux-amd64" then "9391264891f83d188a1b211863410dcea9c9222d137162785818a8e4d8f46947"
-    when "linux-arm64" then "51ab75a3323ebb7ad0198025b728e86c4dbf38293330b3dc05b6f27db99411a2"
-    when "darwin-amd64" then "76ca80e30fba7befe93177661352f7cb00bbb2e43a0b5f343e04f08c5db0c0d5"
-    when "darwin-arm64" then "6f5ddd38604e07cc447dce4cf745023982539bd005abf4974b91ade9c7c6b44a"
+    when "linux-amd64" then "1c601619fbf259728eed937b70f3eec838121a0e5dd1ade3b18f13e128ec70ea"
+    when "linux-arm64" then "efb536137665458816a7302f07a4a4cc91eef0ffe3d0a1e26d5a981e3ab2912c"
+    when "darwin-amd64" then "407f46c82c46be067c7ac8f61c8370565c7209203631d5669610eb955574c4cf"
+    when "darwin-arm64" then "210408915871539fe1a97aa6b95326be3bd362f14d82cb439b44849d5fa99ea2"
     else
       raise "CodeGPT: Unsupported system #{os}-#{arch}"
     end
