daa85a5f3a6ff3f28a39d0647d7e17a9262115bd
Bo-Yi Wu <appleboy.tw@gmail.com>
2024-06-13 05:34:34 +0000

    [skip ci] Updated Version via cron

--- gorush.rb
+++ gorush.rb
@@ -1,7 +1,7 @@
 class Gorush < Formula
   desc "A push notification server written in Go (Golang)."
   homepage "https://github.com/appleboy/gorush"
-  version "1.18.1"
+  version "1.18.2"
 
   os = OS.mac? ? "darwin" : "linux"
   arch = case Hardware::CPU.arch
@@ -16,10 +16,10 @@ class Gorush < Formula
   @@using = :nounzip
 
   @@sha256 = case "#{os}-#{arch}"
-    when "linux-amd64" then "3c6098b3f8ce585874e3c8e00eeaceddc2956feac9d2641ad1ac418100bd9835"
-    when "linux-arm64" then "ca009dbb8ce4a7a2aaccbeed6a7cfca020bfe729648199e2556c52b6632ed4a6"
-    when "darwin-amd64" then "e68b55a445f8d136c4931356df6087df1e2377a584528ba4da34980abb85e434"
-    when "darwin-arm64" then "fa9e2bcffdced811a2886df8cdeb030bcfaa09813f867c08c0e9e7dedb522c35"
+    when "linux-amd64" then "1be961abd4200f9b02ffee5dab76fab16988c112a189ea42c0685f5ee30774ae"
+    when "linux-arm64" then "06b10cd7065b656cf2941e7fe4194e49c64dee7d81243e140bd2684113084689"
+    when "darwin-amd64" then "4dc80c5faa759302f9a57908690c8b279ceaffcf6331a9ebb96af7fee0712f6b"
+    when "darwin-arm64" then "eac61e349c46d282cb60151416d0ea8499fbc813aa651125467913f36245cb88"
     else
       raise "gorush: Unsupported system #{os}-#{arch}"
     end
