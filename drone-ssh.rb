14e08e87b1e23767d279780443b3a67ac6fc002b
Bo-Yi Wu <appleboy.tw@gmail.com>
2025-03-10 05:34:20 +0000

    [skip ci] Updated Version via cron

--- drone-ssh.rb
+++ drone-ssh.rb
@@ -1,7 +1,7 @@
 class DroneSsh < Formula
   desc "Execute commands on a remote host through SSH"
   homepage "https://github.com/appleboy/drone-ssh"
-  version "1.8.0"
+  version "1.8.1"
 
   os = OS.mac? ? "darwin" : "linux"
   arch = case Hardware::CPU.arch
@@ -18,10 +18,10 @@ class DroneSsh < Formula
   depends_on "xz"
 
   @@sha256 = case "#{os}-#{arch}"
-    when "linux-amd64" then "794225f55d6542e569cc6fb02ba10855741db378fe60d517da92b7672c23da29"
-    when "linux-arm64" then "ef6c940839abda77a8c7cec9b3ad81ace7210221a568148f8bcb290191932cda"
-    when "darwin-amd64" then "c1a00985cfa9394b751759dad1591a3916b9adf33e2c641413364c6a53cd98f2"
-    when "darwin-arm64" then "7e4695230066696a04338488a0030ff1c256d1951cb0de390f165b359d750765"
+    when "linux-amd64" then "13b773063cbc4ed6f31ab2306be8a866bacb6de3a72799c6314dd95ea7328962"
+    when "linux-arm64" then "f7c22b4362554cfd8e5556a46560c88400db40c4c5d249084dd4ea499fd61123"
+    when "darwin-amd64" then "02a31ab488e0e6189f4678e00ac1b1b433aa9b59f1fec86779c58ce2558ccbc5"
+    when "darwin-arm64" then "213dc199df647ff10cd8af8babb7f87fae0d74e5462837305dfc4220fb7a41e2"
     else
       raise "drone-ssh: Unsupported system #{os}-#{arch}"
     end
